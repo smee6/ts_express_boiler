b3952034171b3e1566820499b4534570f02e2cb7
smee6 <smee6@naver.com>
2023-03-14 15:55:40 +0900

    모듈 통일

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -2,6 +2,31 @@ const { Test } = require("../models");
 const CONSTANTS = require("../conf/CONSTANTS.js");
 const ERROR = require("../conf/ERROR_CODE.js");
 
+/**
+ * 
+ * @name getTest
+ * @method GET
+ * @url /test
+ * @description Test data 를 다불러옴
+ */
+exports.getTest = async (req, res, next) => {
+    try {
+        const test = await Test.find();
+        res.send(test);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err: err.message });
+    }
+};
+
+/**
+ * 
+ * @name postTest
+ * @method POST
+ * @url /test
+ * @body { name: String, age: Number }
+ * @description Test를 생성하는 API
+ */
 exports.postTest = async (req, res, next) => {
     try {
         const { name, age } = req.body;
--- src/routes/testRouter.js
+++ src/routes/testRouter.js
@@ -2,11 +2,7 @@ const { Router } = require("express");
 const testRouter = Router();
 const testController = require("../controllers/testController");
 
-
-testRouter.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
+testRouter.get("/", testController.getTest);
 testRouter.post("/", testController.postTest);
 
 module.exports = { testRouter };
